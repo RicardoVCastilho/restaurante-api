39b1315dd86d40a85c2eb94dae2b85b16c8f7748
RicardoVCastilho <seuemail@exemplo.com>
2025-06-06 02:12:08 -0300

    fixing bus

--- controller/clienteController.js
+++ controller/clienteController.js
@@ -9,7 +9,7 @@ exports.getAll = (req, res) => {
 
 exports.getById = (req, res) => {
   const { id } = req.params;
-  conn.query('SELECT * FROM Clientes WHERE id = ?', [id], (err, result) => {
+  connection.query('SELECT * FROM Clientes WHERE id = ?', [id], (err, result) => {
     if (err) return res.status(500).send(err);
     res.json(result);
   });
@@ -17,7 +17,7 @@ exports.getById = (req, res) => {
 
 exports.create = (req, res) => {
   const { nome, cidade, telefone } = req.body;
-  conn.query(
+  connection.query(
     'INSERT INTO Clientes (nome, cidade, telefone) VALUES (?, ?, ?)',
     [nome, cidade, telefone],
     (err) => {
@@ -30,7 +30,7 @@ exports.create = (req, res) => {
 exports.update = (req, res) => {
   const { id } = req.params;
   const { nome, cidade, telefone } = req.body;
-  conn.query(
+  connection.query(
     'UPDATE Clientes SET nome = ?, cidade = ?, telefone = ? WHERE id = ?',
     [nome, cidade, telefone, id],
     (err) => {
@@ -42,7 +42,7 @@ exports.update = (req, res) => {
 
 exports.delete = (req, res) => {
   const { id } = req.params;
-  conn.query('DELETE FROM Clientes WHERE id = ?', [id], (err) => {
+  connection.query('DELETE FROM Clientes WHERE id = ?', [id], (err) => {
     if (err) return res.status(500).send(err);
     res.send('Cliente deletado');
   });
